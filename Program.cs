b1f6aa12f956a1a232937d21164ea88cfc5a26b0
Andrew Moore <mooreandrew015@gmail.com>
2021-04-21 14:59:42 -0400

    created a class Dino and made 3 dinos

--- Program.cs
+++ Program.cs
@@ -2,11 +2,43 @@
 
 namespace JurasicPark2
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Dino
     {
-      Console.WriteLine("Welcome to C#");
+        public string Name { get; set; }
+        public string DietType { get; set; }
+        public int Weight { get; set; }
+        public int EnclosureNumber { get; set; }
     }
-  }
+    class Program
+    {
+        static void BannerMessage(string message)
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(message);
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            var Matt = new Dino();
+            Matt.Name = "Matt";
+            Matt.DietType = "Carnivore";
+            Matt.Weight = 500;
+            Matt.EnclosureNumber = 3;
+
+            var James = new Dino();
+            James.Name = "James";
+            James.DietType = "Herbivore";
+            James.Weight = 100;
+            James.EnclosureNumber = 6;
+
+            var Andrew = new Dino();
+            Andrew.Name = "Andrew";
+            Andrew.DietType = "Carnivore";
+            Andrew.Weight = 120;
+            Andrew.EnclosureNumber = 11;
+
+        }
+    }
 }
