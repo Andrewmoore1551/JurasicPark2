a1204013a07c5c04c6bc38e85c6c92c6592748a6
Andrew Moore <mooreandrew015@gmail.com>
2021-04-21 20:45:34 -0400

    still debugging

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace JurasicPark2
 {
@@ -27,18 +29,104 @@ namespace JurasicPark2
             Matt.Weight = 500;
             Matt.EnclosureNumber = 3;
 
-            var James = new Dino();
-            James.Name = "James";
-            James.DietType = "Herbivore";
-            James.Weight = 100;
-            James.EnclosureNumber = 6;
+            var James = new Dino()
+            {
+                Name = "James",
+                DietType = "Herbivore",
+                Weight = 100,
+                EnclosureNumber = 6,
+            };
+            var Andrew = new Dino()
+            {
+                Name = "Andrew",
+                DietType = "Carnivore",
+                Weight = 120,
+                EnclosureNumber = 11,
+            };
+            var dinos = new List<Dino>();
 
-            var Andrew = new Dino();
-            Andrew.Name = "Andrew";
-            Andrew.DietType = "Carnivore";
-            Andrew.Weight = 120;
-            Andrew.EnclosureNumber = 11;
+            dinos.Add(Matt);
+            dinos.Add(James);
+            dinos.Add(Andrew);
 
+            BannerMessage("Welcome to Jurasic Park");
+
+            var userHasChosenToQuit = false;
+
+            while (userHasChosenToQuit == false)
+            {
+                Console.WriteLine("--------------------");
+                Console.WriteLine("Menu:");
+                Console.WriteLine("ADD - Add a new dino");
+                Console.WriteLine("SEE - see all the dinos");
+                Console.WriteLine("TRANSFER - Transfer a dino");
+                Console.WriteLine("REMOVE - Remove a dino");
+                Console.WriteLine("SUMMARY - View amount of herbivores and carnivores");
+
+                Console.WriteLine("-------------------");
+                Console.WriteLine("QUIT _ Quit the app");
+                Console.WriteLine();
+                Console.Write("What is your choice? ");
+                var choice = Console.ReadLine().ToUpper().Trim();
+
+                if (choice == "QUIT")
+                {
+                    userHasChosenToQuit = true;
+                }
+
+                if (choice == "ADD")
+                {
+                    var dino = new Dino();
+                    Console.WriteLine("-------------------");
+                    Console.Write("What is the dinos name? ");
+                    dino.Name = Console.ReadLine();
+                    Console.Write("What is the diet type? ");
+                    dino.DietType = Console.ReadLine();
+                    Console.Write("What is the dinos weight? ");
+                    dino.Weight = int.Parse(Console.ReadLine());
+                    Console.Write("What is the dinos enclosure number? ");
+                    dino.EnclosureNumber = int.Parse(Console.ReadLine());
+                    dinos.Add(dino);
+                }
+                if (choice == "REMOVE")
+                {
+                    Console.Write("What dino would you like to delete? ");
+                    var dinoToDeleteName = Console.ReadLine();
+                    var dinoToDelete = dinos.First(firstdino => firstdino.Name == dinoToDeleteName);
+                    dinos.Remove(dinoToDelete);
+                }
+                // error with transfering 
+                if (choice == "TRANSFER")
+                {
+                    Console.Write("What is the name of the dino you'd like to transfer? ");
+                    var dinoToTransferName = Console.ReadLine();
+                    var dinoToTransfer = dinos.FindIndex(firstdino => firstdino.Name == dinoToTransferName);
+                    Console.Write("Where would you like to tranfer this dino to? ");
+                    var newEnclosureNumber = int.Parse(Console.ReadLine());
+                    dinos[dinoToTransfer].EnclosureNumber = newEnclosureNumber;
+
+
+                }
+                if (choice == "SEE")
+                {
+                    Console.WriteLine("List of dinos");
+                    Console.WriteLine("----------------");
+                    foreach (var dino in dinos)
+                    {
+                        Console.WriteLine($"{dino.Name} is a {dino.DietType} is weighing {dino.Weight}lbs and is in enclosure number {dino.EnclosureNumber}");
+                    }
+
+                }
+                // error displaying how many carnivores and herbivores there is 
+                if (choice == "SUMMARY")
+                {
+                    var carnivores = dinos.Where(Dino => Dino.DietType == "carnivore");
+                    var numberOfCarnivores = carnivores.Count();
+                    var numberOfHerbivores = dinos.Count();
+                    Console.Write($"There are {numberOfCarnivores} number of carnivores. There are {numberOfHerbivores} number of herbivores");
+                }
+            }
+            BannerMessage("Goodbye");
         }
     }
 }
